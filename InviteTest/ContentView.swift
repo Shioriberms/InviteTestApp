75850bc390da57677c0ff398aac06af5d73814a5
Shiori <shiori.bermudo@gmail.com>
2021-11-27 19:02:48 +0800

    Initial Commit

---
+++ InviteTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InviteTest
+//
+//  Created by user210003 on 11/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InviteTest/InviteTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  InviteTestApp.swift
+//  InviteTest
+//
+//  Created by user210003 on 11/27/21.
+//
+
+import SwiftUI
+
+@main
+struct InviteTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
